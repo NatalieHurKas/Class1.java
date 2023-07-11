f1d09702006f9c53bf2367f1278b7cd54e20723a
NatalieHurKas <139253031+NatalieHurKas@users.noreply.github.com>
2023-07-11 19:48:31 +0300

    Update Class2.java

--- src/Sel1605/Class2.java
+++ src/Sel1605/Class2.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+//testing testing
 import java.util.List;
 
 public class Class2 {
